e60da5ec2dafbcb5acaebb60459dc584b83b3431
lucus lee <lixin9311@gmail.com>
2022-01-22 09:04:30 +0000

    fix get default port

--- utils/utils.go
+++ utils/utils.go
@@ -13,7 +13,7 @@ const (
 func GetDefaultPort(serviceType string, defaultPort int) int {
 	if strings.EqualFold(os.Getenv(envServiceType), serviceType) {
 		if portStr := os.Getenv("PORT"); portStr != "" {
-			if port, err := strconv.Atoi(portStr); err != nil {
+			if port, err := strconv.Atoi(portStr); err == nil {
 				return port
 			}
 		}
